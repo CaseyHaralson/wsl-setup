adcfaac4a768c0061bab3d1089e3243d486a512a
Casey <casey.haralson@gmail.com>
2023-09-19 21:00:42 -0600

    adding to mamba config

--- scripts/mamba.sh
+++ scripts/mamba.sh
@@ -13,6 +13,12 @@ if [ -z "$MAMBA_LOCATION" ]; then
   cd $TEMP_DIR
   curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh"
   bash Mambaforge-Linux-x86_64.sh 
+
+  AUTO_ACTIVATE_BASE=`cat ~/mambaforge/.condarc | grep auto_activate_base`
+  if [ -z "$AUTO_ACTIVATE_BASE" ]; then
+    echo "auto_activate_base: false" >> ~/mambaforge/.condarc
+  fi
+
   echo ""
   echo "Mamba was installed. The shell needs to be reloaded."
   echo ""
